a5ff42642b8d7dc6e6249a1c54b7df7a297998d1
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-14 19:52:59 -0300

    Tipando objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function welcome(request: Request, response: Response) {
+    const user = createUser({
+        name: 'Thiago', 
+        email: 'thiagofalmd@gmail.com', 
+        password: '123',
+        techs: [
+            'Node.js', 
+            'PHP', 
+            'React',
+            { title: 'Javascript', experience: 100 }
+        ]
+    });
+
     return response.json({ message: 'Welcome Typescript!' })
 }
